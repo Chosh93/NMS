5a9088c82efd7604cd5185f60e34dd04475dd364
Chosh93 <chosh930911@gmail.com>
2024-04-11 23:09:11 +0900

    NMS CPU, Memory 가져오기 완료

--- src/main/java/com/example/nms/NmsApplication.java
+++ src/main/java/com/example/nms/NmsApplication.java
@@ -18,8 +18,10 @@ public class NmsApplication {
 			for(int i=0; i < 100; i++){
 				System.out.println("===================");
 				System.out.println("CPU Usage : " + String.format("%.2f", osBean.getSystemCpuLoad() * 100 ));
+				// cpu 사용량은 0~1사이의 값으로 가져오므로 * 100 으로 백분율로 출력
 				System.out.println("Memory Free Space : " + String.format("%.2f", (double)osBean.getFreePhysicalMemorySize()/1024/1024/1024));
-				System.out.println("Memory Total Space : " + String.format("%.2f", (double)osBean.getTotalPhysicalMemorySize()/1024/1024/1024  ));
+				System.out.println("Memory Total Space : " + String.format("%.2f", (double)osBean.getTotalPhysicalMemorySize()/1024/1024/1024));
+				// 메모리를 가져올때 바이트 단위로 가져오기 때문에 1024로 세번 나눠 킬로 -> 메가 -> 기가로 변환
 
 				Thread.sleep(1000);
 			}
